9ab2f60a19b8ba306ef2dfa34b52f727fd011098
Eric Yun <yunseric@gmail.com>
2024-10-10 03:23:01 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+import { LatestPost } from "~/app/_components/post";
+import { getServerAuthSession } from "~/server/auth";
+import { api, HydrateClient } from "~/trpc/server";
+
+export default async function Home() {
+  // const hello = await api.post.hello({ text: "from tRPC" });
+  // const session = await getServerAuthSession();
+
+  void api.post.getLatest.prefetch();
+
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        testing
+      </main>
+    </HydrateClient>
+  );
+}
