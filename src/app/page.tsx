cd5c78ef7a39dce55f5421915c6a750c6d671c33
Eric Yun <yunseric@gmail.com>
2024-11-14 02:59:40 -0800

    fix: got image urls to work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+import Image from "next/image";
 import { api, HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  // const hello = await api.post.hello({ text: "from tRPC" });
-  // const session = await getServerAuthSession();
-
+  const cards = await api.card.getCards();
 
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        testing
+        {cards?.map((card) => (
+          <div key={card.id}>
+            <div key={card.id}>{card.name}</div>
+            {card.Image?.image_url && (
+              <Image
+                src={card.Image?.image_url as string}
+                alt="card image"
+                height={100}
+                width={100}
+              />
+            )}
+          </div>
+        ))}
       </main>
     </HydrateClient>
   );
--- src/server/api/routers/card.ts
+++ src/server/api/routers/card.ts
@@ -5,18 +5,15 @@ import {
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
-import { db } from "~/server/db";
-import type { Card } from "@prisma/client"
-
 
 export const cardRouter = createTRPCRouter({
-  getAllCards: publicProcedure.query(async (): Promise<Card[]> => {
-    try {
-      const myCards = await db.card.findMany();
-      return myCards;
-    } catch (error) {
-      console.error("Error fetching cards:", error);
-      return [];
-    }
-  })
+  getCards: publicProcedure.query(async ({ ctx }) => {
+    const cards = await ctx.db.card.findMany({
+      take: 50,
+      include: {
+        Image: true
+      }
+    });
+    return cards;
+  }),
 });
