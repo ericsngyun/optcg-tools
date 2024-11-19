983fbfa8644e856f22f2165fcc7475d4966f30b4
Eric Yun <yunseric@gmail.com>
2024-11-19 00:55:23 -0800

    chore: fix responsive lag issue with max width wrapper

--- src/app/_components/MaxWidthWrapper.tsx
+++ src/app/_components/MaxWidthWrapper.tsx
@@ -1,8 +1,23 @@
 import React from "react"
-export default function MaxWidthWrapper({children}) {
-  return (
-    <div className="mx-4">
+import { cn } from "~/lib/utils";
+import type { ReactNode } from "react"
+
+const MaxWidthWrapper = ({
+  className,
+  children
+}: {
+  className?: string,
+  children: ReactNode
+}) => {
+
+  return(
+    <div className = {cn(
+      "mx-auto w-full max-w-screen-2xl px-2.5 md:px-20", 
+      className
+    )} >
       {children}
     </div>
   )
 }
+
+export default MaxWidthWrapper;
--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { api, HydrateClient } from '~/trpc/server'
 import Card from '../_components/Card'
+import MaxWidthWrapper from '../_components/MaxWidthWrapper'
 
 export default async function cards() {
   const cards = await api.card.getCards()
 
   return (
-    <div className="grid grid-cols-5 gap-2">
-      {cards?.map((card) => (
-          <Card 
-            key={card.id}
-            card={card}
-          />
-        ))}
-    </div>
+    <MaxWidthWrapper>
+      <div className="grid grid-cols-5 gap-2">
+        {cards?.map((card) => (
+            <Card 
+              key={card.id}
+              card={card}
+            />
+          ))}
+      </div>
+    </MaxWidthWrapper>
   )
 
 }
