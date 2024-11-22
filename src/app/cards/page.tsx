aa4bfcc2e318cd949cec0d234a472ff086178920
Eric Yun <yunseric@gmail.com>
2024-11-22 00:36:53 -0800

    feat: added form component

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -15,13 +15,15 @@ export default async function cards() {
   const cards = await api.card.getCards();
 
   return (
-    <div className="mx-auto w-full max-w-screen-2xl px-2.5 md:px-12 lg:px-20 xl:px-28 gap-12 py-8">
+    <div className="mx-auto w-full max-w-screen-2xl px-2.5 md:px-12 lg:px-20 xl:px-28 space-y-12 py-12">
       {/* this is for the filter box */}
-      <Card>
+      <Card className="">
+        <CardHeader>
+          <CardTitle>Card List</CardTitle>
+          <CardDescription>Total Results: {cards.length}</CardDescription>
+        </CardHeader>
         <CardContent>
-          <CardTitle>
-            Filter
-          </CardTitle>
+          
         </CardContent>
       </Card>
       <div className="grid grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
